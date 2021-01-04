95da38096a835197f6fc97b805c2dc439be588aa
pegasusVikas <vikas.vik934@gmail.com>
2021-01-04 10:21:12 +0530

    null receipt bug fixed

--- routes/Transaction.js
+++ routes/Transaction.js
@@ -6,11 +6,11 @@ const Tx =require("ethereumjs-tx").Transaction;
 //const {WeiToRs,convert} =require("../converter")
 
 router.post('/', async (req, res) => {
-    var wallet1 = {
+    var wallet2 = {
       address: '0x9027a33b4917e17D032F509eAbDBe7193E2Bb0eB',
       privateKey: '0x5d2e44bf4769b98371f2befaebec6a478d1f87063e9413b8675552b00f6ae65a'
     }
-    var wallet2 = {
+    var wallet1 = {
       address: '0x167cc08C0722710126c5c563Ca8E03ebef5D7D3a',
       privateKey: '0x2ed6138cddc59b2a48529d659898b9a2fe22e045a78989f388faf5530d8bb853'
     }
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
     var amount = req.body.amount;//Rupee
     axios.get(`https://min-api.cryptocompare.com/data/price?fsym=INR&tsyms=ETH`)
     .then((resp)=>{
-     amount=amount*resp.data["ETH"];
+     amount=(amount*resp.data["ETH"]).toFixed(18); //float point precsion
   
     if (wallet1.privateKey.slice(0, 2) === "0x") {
   
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
         
       if(err){
       res.status=501;
-      res.json(err);
+      res.json({err:err});
       }
       else{
       const txObject = {
@@ -54,19 +54,16 @@ router.post('/', async (req, res) => {
       const raw = "0x" + serializedTransaction.toString('hex');
   
       //send transaction
-      web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-        if (err)
-        {
-          res.status=501;
-          res.json({err:err.message});
-        }else{
-        console.log('txHash', txHash);
-        
-        //fetching transaction details from hash
-        web3.eth.getTransaction(`${txHash}`)
-        .then((receipt)=>{
-            //console.log(receipt);
-            var json={ INR:{},ETH:{}}
+      web3.eth.sendSignedTransaction(raw)
+      .on('receipt',(receipt)=>{
+        console.log(receipt);
+            if(!receipt){
+            console.log("no receipt")
+              res.json({err:"no receipt returned"})
+              res.status=501;
+            }
+            var json={ INR:{},ETH:{},reached:true};
+            console.log("this is receipt",receipt);
             axios.get(`https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=INR`)
             .then((ETI)=>{
 
@@ -76,18 +73,26 @@ router.post('/', async (req, res) => {
 
             //converting Wei to Rupee
             ETI=ETI.data["INR"];
-            json.INR.amount=WeiToRs(receipt.value);
-            json.INR.tax=WeiToRs(receipt.gas*receipt.gasPrice)
+            value=parseInt(txObject.value,16);
+            gasLimit=parseInt(txObject.gasLimit,16);
+            gasPrice=parseInt(txObject.gasPrice,16);
+
+            json.INR.amount=WeiToRs(value);
+            json.INR.tax=WeiToRs(gasLimit*gasPrice)
             json.ETH=receipt;
+            json.ETH.value=value
+            json.ETH.gasLimit=gasLimit
+            json.ETH.gasPrice=gasPrice
+
             res.json(json);
             
+            }).catch((err)=>{console.log(err);res.status=501;res.json({err:err.message})})
             
-            }).catch((err)=>res.json(err))
-            
-            
-        }).catch((err)=>res.json(err));
-      }
       })
+      .on('transactionHash',(txHash)=>{
+        console.log('txHash', txHash);
+      })
+      .on('error', (err)=>{console.error(err);res.status=501;res.json({err:err.message})})
     }
     })
    })
